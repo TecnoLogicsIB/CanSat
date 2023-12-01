08f96380bf455d3dd6cecc7d574034e12de8f76a
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-12-01 08:06:30 +0100

    Update Prova_SD.ino

--- Prova_SD.ino
+++ Prova_SD.ino
@@ -1,24 +1,24 @@
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
+#include "FS.h"   // per gestionar arxius
+#include "SD.h"   // per treballar amb SD
+#include "SPI.h"  // per traballar amb SDI
 
 void setup() 
 {
-  SD.begin(5);
+  SD.begin(5);   // inicialització de la SD indicant el pin CS
   //writeFile(SD, "/hello.txt", "Hello \n");
-  writeFile(SD, "/dades.csv", "Hello \n");
+  writeFile(SD, "/dades.csv", "Hello \n");  
 }
 
 void loop() 
 {
   //appendFile(SD, "/hello.txt", "World!\n");
-  appendFile(SD, "/dades.csv", "World!\n");
+  appendFile(SD, "/dades.csv", "World!\n");  
   delay(1000);
 }
 
 // ........................
 
-void writeFile(fs::FS &fs, const char * path, const char * message)
+void writeFile(fs::FS &fs, const char * path, const char * message)  // escriu contingut en un arxiu. Si l'arxiu no existeix el crearà
 {
   //Serial.printf("Writing file: %s\n", path);
   File file = fs.open(path, FILE_WRITE);
@@ -26,7 +26,7 @@ void writeFile(fs::FS &fs, const char * path, const char * message)
   file.close();
 }
 
-void appendFile(fs::FS &fs, const char * path, const char * message)
+void appendFile(fs::FS &fs, const char * path, const char * message)  // afegeix contingut a l'arxiu especificat
 {
   //Serial.printf("Appending to file: %s\n", path);
   File file = fs.open(path, FILE_APPEND);
@@ -34,13 +34,13 @@ void appendFile(fs::FS &fs, const char * path, const char * message)
   file.close();
 }
 
-void deleteFile(fs::FS &fs, const char * path)
+void deleteFile(fs::FS &fs, const char * path)  // elimina l'arxiu especificat
 {
   //Serial.printf("Deleting file: %s\n", path);
   fs.remove(path);
 }
 
-void readFile(fs::FS &fs, const char * path)
+void readFile(fs::FS &fs, const char * path)  // mostra el contingut de l'arxiu epl monitor sèrie
 {
   Serial.printf("Reading file: %s\n", path);
   File file = fs.open(path);
