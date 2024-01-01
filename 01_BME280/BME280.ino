b15024867cbcd475865d81fb7a6b95a6d0933348
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2024-01-01 19:24:47 +0100

    Update BME280.ino

--- 01_BME280/BME280.ino
+++ 01_BME280/BME280.ino
@@ -1,1 +1,43 @@
+// inserció de llibreries
+#include <Wire.h>
+#include <Adafruit_BME280.h>
 
+// creació d'objectes i variables:
+Adafruit_BME280 bme; 
+float temperatura, pressio, altitud, humitat;
+unsigned long temps_referencia = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  bme.begin(0x76);
+}
+
+void loop() 
+{
+  if((millis()-temps_referencia)>=1000)
+  {
+    temps_referencia=millis();
+    llegeix_sensors();
+    mostra_dades();
+  }
+}
+
+// ...............................
+
+void llegeix_sensors()
+{
+  temperatura=bme.readTemperature();
+  pressio=bme.readPressure()/100;
+  altitud=bme.readAltitude(1013.25);
+  humitat=bme.readHumidity();
+}
+
+void mostra_dades()
+{
+  Serial.println (temperatura);
+  Serial.println (pressio);
+  Serial.println (altitud);
+  Serial.println (humitat);
+  Serial.println ();
+}
