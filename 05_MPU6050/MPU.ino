6d52d18d48d16cfca98a25fd177e8688ba60ec00
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2024-03-03 06:44:16 +0100

    Update MPU.ino

--- 05_MPU6050/MPU.ino
+++ 05_MPU6050/MPU.ino
@@ -99,3 +99,43 @@ void envia_RF()
   SerialRF.println(missatge.c_str());  // missatge a enviar per RF
   //Serial.println (missatge);  // per comprovació
 }
+
+// ----------------------- FUNCIONS SD ----------------------
+
+void writeFile(fs::FS &fs, const char * path, const char * message)
+{
+  //Serial.printf("Writing file: %s\n", path);
+  File file = fs.open(path, FILE_WRITE);
+  file.print(message);
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message)
+{
+  //Serial.printf("Appending to file: %s\n", path);
+  File file = fs.open(path, FILE_APPEND);
+  file.print(message);
+  file.close();
+}
+
+void deleteFile(fs::FS &fs, const char * path)
+{
+  Serial.printf("Deleting file: %s\n", path);
+  fs.remove(path);
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
